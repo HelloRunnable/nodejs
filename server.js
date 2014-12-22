a2d57335dc20e76306ebf1750dba07973e37b155
HelloRunnable <github@runnable.com>
2014-12-22 14:04:39 -0800

    Update server.js to "Hello World"

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello Runnable!');
+    res.send('Hello World!');
     console.log("Requested route: /");
 });
 
